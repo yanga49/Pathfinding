f2a7862d55b24d15bbaa1f1ae8067480c4318a9a
Akanksha <akanksha@Akankshas-MacBook-Air.local>
2022-09-20 20:00:12 -0400

    updated plotter

--- Plotter.py
+++ Plotter.py
@@ -1,5 +1,3 @@
-import math
-import random
 import matplotlib.pyplot as plt
 
 # given a list of y values, this function plots it using matplotlib in a visualizable way
@@ -8,7 +6,6 @@ class Plotter:
         pass
 
     def plot (self, y: list, title = '', x_title = '', y_title = ''):
-        plt.figure(dpi=150)
         plt.plot(y)
         plt.ylabel(y_title)
         plt.suptitle(title)
