e624644738324c619294126ee21f3b26ab2f7dcc
Anna Yang <yanga49@mcmaster.ca>
2022-09-21 08:44:24 -0400

    changed dijkstra, a_star, priority_queue

--- Graph_Algorithms/A_star.py
+++ Graph_Algorithms/A_star.py
@@ -7,29 +7,20 @@ from Graph_Algorithms.Priority_Queue import PriorityQueue
 
 
 class A_star(ShortestPath):
-
-    def shortest_path2(self, graph, from_node_id, to_node_id):
-        shortest_paths = self.A_star(graph, from_node_id, to_node_id)
-        edge_to = shortest_paths[0]
-        dist_to = shortest_paths[1]
-        path = []
-        node = to_node_id
-        while node != from_node_id:
-            path.append(node)
-            node = edge_to[node]
-        path.append(from_node_id)
-        print("shortest path length = ", dist_to[to_node_id])
-        string_path = [str(i) for i in path]
-        print(" -> ".join(reversed(string_path)))
     def shortest_path(self, graph: Graph, from_node_id, to_node_id):
         # provide A* implementation here
         unvisited = PriorityQueue()
-        unvisited.insert(from_node_id, graph.get_max_weight() + distance(graph, graph.get_node(from_node_id), graph.get_node(to_node_id)))
+        unvisited.insert(from_node_id, - distance(graph.get_node(from_node_id), graph.get_node(to_node_id)))
         edge_to = {}
         dist_to = {}
+        m = 0
+        for i in graph.all_nodes:
+            dist_to[i] = float('inf')
         edge_to[from_node_id] = None
         dist_to[from_node_id] = 0
+        n = 0
         while not unvisited.is_empty():
+            n += 1
             current = unvisited.pop()[0]
             if current == to_node_id:
                 break
@@ -37,14 +28,17 @@ class A_star(ShortestPath):
             for adj in adjacent:
                 temp = dist_to[current] + graph.get_node(current).get_weight(adj)
                 a = adj.get_node_id()
-                if adj not in dist_to or temp < dist_to[a]:
+                if a not in edge_to or temp < dist_to[a]:
                     dist_to[a] = temp
                     edge_to[a] = current
-                    unvisited.insert(a, graph.get_max_weight() - graph.get_node(current).get_weight(adj) + distance(graph, graph.get_node(a), graph.get_node(to_node_id)))
+                    m += 1
+                    unvisited.insert(a, temp + distance(graph.get_node(a), graph.get_node(to_node_id)))
+        print("num of inserts = ", m)
+        print("num of visited = ", n)
         return edge_to, dist_to
 
 
-def distance(graph: Graph, from_node_id: Station_Node, to_node_id: Station_Node):
+def distance(from_node_id: Station_Node, to_node_id: Station_Node):
     x = abs(from_node_id.lat - to_node_id.lat)
     y = abs(from_node_id.long - to_node_id.long)
     return math.sqrt(x * x + y * y)
--- Graph_Algorithms/Dijkstra.py
+++ Graph_Algorithms/Dijkstra.py
@@ -7,12 +7,17 @@ class Dijkstra(ShortestPath):
 
     def shortest_path(self, graph: Graph, from_node_id, to_node_id):
         unvisited = PriorityQueue()
-        unvisited.insert(from_node_id, graph.get_max_weight())
+        unvisited.insert(from_node_id, float('-inf'))
         edge_to = {}
         dist_to = {}
+        m = 0
+        n = 0
+        for i in graph.all_nodes:
+            dist_to[i] = float('inf')
         edge_to[from_node_id] = None
         dist_to[from_node_id] = 0
         while not unvisited.is_empty():
+            n += 1
             current = unvisited.pop()[0]
             if current == to_node_id:
                 break
@@ -20,8 +25,11 @@ class Dijkstra(ShortestPath):
             for adj in adjacent:
                 temp = dist_to[current] + graph.get_node(current).get_weight(adj)
                 a = adj.get_node_id()
-                if adj not in dist_to or temp < dist_to[a]:
+                if a not in edge_to or temp < dist_to[a]:
                     dist_to[a] = temp
                     edge_to[a] = current
-                    unvisited.insert(a, graph.get_max_weight() - graph.get_node(current).get_weight(adj))
+                    unvisited.insert(a, temp)
+                    m += 1
+        print("num of inserts = ", m)
+        print("num of visited = ", n)
         return edge_to, dist_to
