74064b0ea8ccfe5ef7f4808451407b899880e7c7
Anna Yang <yanga49@mcmaster.ca>
2022-10-02 14:57:48 -0400

    added test cases for heuristic and patrol planner

--- test_graphlab.py
+++ test_graphlab.py
@@ -5,18 +5,9 @@ from Metric_Extractor import Metric_Extractor
 from Itinerary import Itinerary
 from Graph_Algorithms.Dijkstra import Dijkstra
 from Graph_Algorithms.A_star import A_star
+from PatrolPlanner import PatrolPlanner
+from Station_Node import Station_Node
 # from Graph_Algorithms.Priority_Queue import PriorityQueue
-# from ShortestPath import ShortestPath
-# from Test import Test
-# from KPIs.CpuTime import CpuTime
-# from KPIs.ExecutionTime import ExecutionTime
-# from KPIs.Visited import Visited
-# from KPIs.Inserts import Inserts
-# from KPIs.Compares import Compares
-# from Benchmark import Benchmark
-# from Plotter import Plotter
-# import matplotlib.pyplot as plt
-# import time
 # import pytest
 
 # build test graph 1
@@ -34,12 +25,31 @@ test_graph1.add_edge(6, 8, 2, 'yellow')
 test_graph1.add_edge(8, 9, 3, 'blue')
 test_graph1.add_edge(9, 10, 2, 'blue')
 
-# built test graph 2 (london subway graph)
+# build test graph 2 (london subway graph)
 csv_reader = Csv_reader()
 test_graph2 = Graph()
 graph2 = Graph_builder(csv_reader, test_graph2)
 graph2.create_station_nodes('_dataset/london.stations.csv')
 graph2.create_connections('_dataset/london.connections.csv')
+
+# build test graph 3
+test_graph3 = Graph()
+test_graph3.add_edge(0, 2, 20)
+test_graph3.add_edge(0, 1, 10)
+test_graph3.add_edge(1, 4, 10)
+test_graph3.add_edge(1, 3, 50)
+test_graph3.add_edge(2, 3, 20)
+test_graph3.add_edge(4, 3, 20)
+test_graph3.add_edge(4, 5, 1)
+test_graph3.add_edge(5, 3, 2)
+test_graph3.add_edge(17, 3, 50)
+test_graph3.add_edge(12, 17, 50)
+test_graph3.add_edge(9, 17, 13)
+test_graph3.add_edge(9, 23, 3)
+test_graph3.add_edge(5, 30, 47)
+test_graph3.add_edge(30, 4, 50)
+test_graph3.add_edge(5, 45, 89)
+test_graph3.add_edge(30, 3, 1)
 
 
 def test_extract_csv():
@@ -76,6 +86,18 @@ def test_a_star():
     assert lines == ['10', '10', '10']
 
 
+def test_heuristic():
+    from_node = Station_Node(1, "from")
+    from_node.set_lat(0)
+    from_node.set_long(3)
+    to_node = Station_Node(2, "to")
+    to_node.set_lat(4)
+    to_node.set_long(0)
+    heuristic = A_star()
+    dist = heuristic.distance(from_node, to_node)
+    assert dist == 5
+
+
 def test_same_node():
     itinerary = Itinerary(test_graph1, 1, 1, Dijkstra())
     path = itinerary.find_shortest_path()['path']
@@ -88,6 +110,24 @@ def test_same_node():
     assert lines == []
 
 
+def test_station_node():
+    station = Station_Node(1, "test station")
+    station.set_lat(2)
+    station.set_long(3)
+    station.set_zone(4)
+    station.set_total_lines(5)
+    station.set_is_rail(0)
+    station.set_disp_name("test")
+    assert station.name == "test station"
+    assert station.get_node_id() == 1
+    assert station.lat == 2
+    assert station.long == 3
+    assert station.zone == 4
+    assert station.total_lines == 5
+    assert station.is_rail == 0
+    assert station.disp_name == "test"
+
+
 def test_metric_extractor():
     metric = Metric_Extractor(test_graph1)
     node_count = metric.get_node_count()
@@ -98,3 +138,10 @@ def test_metric_extractor():
     assert edge_count == 12
     assert degree == 4
     assert avg_degree == 2.4
+
+
+def test_patrol_planner():
+    patrol = PatrolPlanner(test_graph3, [0, 2, 30, 17], 0)
+    patrol_path = patrol.find_patrol_path()
+    assert patrol_path[0] == [0, 30, 2, 3, 17, 3, 5, 4, 1, 0]
+    assert patrol_path[1] == 188
